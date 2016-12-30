0c8588f453ae695431984dede86ca6ec3932afa2
DerVerruckteFuchs <derverrucktefuchs@gmail.com>
2016-12-30 06:12:10 -0500

    change to 60fps

--- config.def.h
+++ config.def.h
@@ -45,7 +45,7 @@ static int allowaltscreen = 1;
 
 /* frames per second st should at maximum draw to the screen */
 static unsigned int xfps = 120;
-static unsigned int actionfps = 30;
+static unsigned int actionfps = 60;
 
 /*
  * blinking timeout (set to 0 to disable blinking) for the terminal blinking
